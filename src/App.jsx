93748a3439e14dd5d2fefee2c81f10fc0c971f53
prndP <paranda@gmail.com>
2017-02-28 02:28:41 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Menu from './components/Menu/Menu';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Menu></Menu>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/libs/AnimateCommon.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+const createjs = require('exports?createjs!imports?this=>global!./createjs-2015.11.26.min.js');
+const lib = {};
+const images = {};
+const spritesheet = {};
+export {lib, images, createjs, spritesheet};
