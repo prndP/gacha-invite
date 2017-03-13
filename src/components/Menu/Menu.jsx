ea542e339862f27a2373611e84467cc2726d5941
prndP <paranda@gmail.com>
2017-03-13 02:28:30 -0400

    Moved transitions to be a generic property of modals. Added white button style

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import CogContainer from '../CogContainer/CogContainer';
 import Modal from '../Modal/Modal';
 import menuBgm from './bgm_gacha_menu.mp3';
 import './Menu.styl';
-
 import SSButton from '../SSButton/SSButton';
-
 const GachaImage = require('../../assets/gachabanner.jpg');
-
-function FirstChild(props) {
-    const childrenArray = React.Children.toArray(props.children);
-    return childrenArray[0] || null;
-}
 
 class Menu extends Component {
     render() {
@@ -24,50 +16,40 @@ class Menu extends Component {
                 </audio>
                 <CogContainer>
                     <div className="VerticalCenter">
-                        <ReactCSSTransitionGroup
-                            transitionName="example"
-                            component={FirstChild}
-                            transitionEnter={false}
-                            transitionLeave={false}
-                            transitionAppear={true}
-                            transitionAppearTimeout={200}
-                        >
-                            <Modal className="GachaModal" slide={true}>
-                                <div className="GachaLeft">
+                        <Modal className="GachaModal" transition='Modal-Slide'>
+                            <div className="GachaLeft">
+                                <div className="LimitedContainer">
+                                    <div className="LimitedBox">RSVP By</div>
+                                    <div className="LimitedLabel">06/24 23:59</div>
+                                </div>
+                                <div className="GachaImageContainer">
+                                    <img className="GachaImage" src={GachaImage} role="presentation"/>
+                                    <SSButton className="DetailsButton Pink">
+                                        About Us
+                                    </SSButton>
+                                </div>
+                            </div>
+                            <div className="GachaRight">
+                                <h2>M@GIC WEDDING STORY!</h2>
+                                <p>
+                                    Li popped the question! and now we would like to ask you to save the date for
+                                    our wedding! Li and
+                                    Andrea are having a beautiful daytime wedding on 9/24/17 in Belleville, NJ.
+                                    Please pencil in our
+                                    date so that you can attend! There will be a ceremony, reception, and after
+                                    party!
+                                </p>
+                                <div className="TicketContainer">
+                                    <SSButton className="GachaPullButton Pink with-bg">Save The Date</SSButton>
                                     <div className="LimitedContainer">
-                                        <div className="LimitedBox">RSVP By</div>
-                                        <div className="LimitedLabel">06/24 23:59</div>
-                                    </div>
-                                    <div className="GachaImageContainer">
-                                        <img className="GachaImage" src={GachaImage} role="presentation"/>
-                                        <SSButton className="DetailsButton">
-                                            About Us
-                                        </SSButton>
-                                    </div>
-                                </div>
-                                <div className="GachaRight">
-                                    <h2>M@GIC WEDDING STORY!</h2>
-                                    <p>
-                                        Li popped the question! and now we would like to ask you to save the date for
-                                        our wedding! Li and
-                                        Andrea are having a beautiful daytime wedding on 9/24/17 in Belleville, NJ.
-                                        Please pencil in our
-                                        date so that you can attend! There will be a ceremony, reception, and after
-                                        party!
-                                    </p>
-                                    <div className="TicketContainer">
-                                        <div className="LimitedContainer">
-                                            <div className="LimitedBox">Save The Date Ticket</div>
-                                            <div className="LimitedLabel">
-                                                1
-                                            </div>
+                                        <div className="LimitedBox">Save The Date Tickets</div>
+                                        <div className="LimitedLabel">
+                                            <span className="Number">1</span> ticket
                                         </div>
-                                        <SSButton className="GachaPullButton with-bg">Save The Date</SSButton>
                                     </div>
                                 </div>
-                            </Modal>
-                        </ReactCSSTransitionGroup>
-
+                            </div>
+                        </Modal>
                     </div>
                 </CogContainer>
             </div>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import './Modal.css';
 
+function FirstChild(props) {
+    const childrenArray = React.Children.toArray(props.children);
+    return childrenArray[0] || null;
+}
+
 class Modal extends Component {
-	render() {
-		return (<div className={`Modal ${this.props.className}`}>{this.props.children}</div>);
-	}
+
+    wrapTransition(node) {
+        if (this.props.transition) {
+            return (<ReactCSSTransitionGroup
+                    transitionName={this.props.transition}
+                    component={FirstChild}
+                    transitionEnter={false}
+                    transitionLeave={false}
+                    transitionAppear={true}
+                    transitionAppearTimeout={200}
+                >{node}</ReactCSSTransitionGroup>
+            )
+        }
+        return node;
+    }
+
+    render() {
+        return this.wrapTransition(
+            <div className={`Modal ${this.props.className}`}>{this.props.children}</div>
+        );
+    }
 }
 
 export default Modal;
